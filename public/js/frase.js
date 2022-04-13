c40b286c96073cc628885da21cebaf126a86e52d
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-04-12 23:57:09 -0300

    separado funcao em duas

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,8 +1,12 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
-  $.get("http://localhost:3000/frases", function (data) {
+  $.get("http://localhost:3000/frases");
+}
+
+function trocaFraseAleatoria() {
+  function (data) {
     var frase = $(".frase");
     frase.text(data[0]);
-  });
+  }
 }

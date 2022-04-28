751dd72950d12d2d7401d069c102bc6572f633cb
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-04-28 11:34:58 -0300

    finalizado metodo de frase aleatoria

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,12 +1,10 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
-  $.get("http://localhost:3000/frases");
+  $.get("http://localhost:3000/frases", trocaFraseAleatoria);
 }
 
-function trocaFraseAleatoria() {
-  function (data) {
-    var frase = $(".frase");
-    frase.text(data[0]);
-  }
+function trocaFraseAleatoria(data) {
+  var frase = $(".frase");
+  frase.text(data[0]);
 }

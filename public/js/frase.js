eb9fe2d1f464b1d607955d0ecc5b7ff963788744
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-05-03 15:25:47 -0300

    criado metodo busca frase e troca frase utilizando um id

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
   $("#spinner").toggle();
@@ -20,4 +21,30 @@ function trocaFraseAleatoria(data) {
   frase.text(data[numeroAleatorio].texto);
   atualizaTamanhoFrase();
   atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase() {
+  $("#spinner").toggle();
+  var fraseId = $("#frase-id").val();
+  var dados = {
+    id: fraseId
+  };
+
+  $.get("http://localhost:3000/frases", dados, trocaFase)
+    .fail(function () {
+      $("#erro").toggle();
+      setTimeout(function () {
+        $("#erro").toggle();
+      }, 3000);
+    })
+
+    .always(function () {
+      $("#spinner").toggle();
+    });
+}
+
+function trocaFase(data) {
+  var frase = $(".frase");
+  frase.text(data.texto);
+  atualizaTamanhoFrase(data.tempo);
 }
